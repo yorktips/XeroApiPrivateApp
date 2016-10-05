870100b5e51df28a23099f1d879c9766bc23d77a
york.chen <york.tips@gmail.com>
2016-10-04 23:33:17 -0400

    Added creating account method

--- src/main/java/com/isis/util/xero/XeroClientUtil.java
+++ src/main/java/com/isis/util/xero/XeroClientUtil.java
@@ -1,6 +1,12 @@
 package com.isis.util.xero;
 
+import java.util.List;
+
+import org.apache.log4j.Logger;
+
 public class XeroClientUtil {
+
+	static Logger logger = Logger.getLogger(XeroClientUtil.class.getName());
 
 	public XeroClientUtil() {}
 
@@ -51,6 +57,50 @@ public class XeroClientUtil {
 					return account;
 			}
 		}
+		return null;
+	}
+
+	public static int getMaxAccountCode(ArrayOfAccount arrayOfExistingAccounts) {
+		int nMax=1;
+		if (arrayOfExistingAccounts != null && arrayOfExistingAccounts.getAccount() != null) {
+			for (Account account : arrayOfExistingAccounts.getAccount()) {
+				if (Integer.parseInt(account.getCode()) > nMax)
+					nMax=Integer.parseInt(account.getCode());
+			}
+		}
+		return nMax;
+	}
+	
+	public static Account getPayableAccount(XeroClient xeroClient, ArrayOfAccount arrayOfExistingAccounts, String name) {
+		if (arrayOfExistingAccounts != null && arrayOfExistingAccounts.getAccount() != null) {
+			for (Account account : arrayOfExistingAccounts.getAccount()) {
+				if (account.getName().equalsIgnoreCase(name))
+					return account;
+			}
+		}
+		int nMax=getMaxAccountCode(arrayOfExistingAccounts);
+		nMax++;
+		logger.debug("Creating a new Account. code=" + String.valueOf(nMax));
+		ArrayOfAccount aOA= new ArrayOfAccount();
+        List<Account> accounts = aOA.getAccount();
+
+		Account account = new Account();
+		account.setName(name);
+		account.setType(AccountType.REVENUE);
+		account.setEnablePaymentsToAccount(true);
+		account.setCode(String.valueOf(nMax));
+		
+		accounts.add(account);
+		try{
+			xeroClient.postBeans(aOA);
+			accounts = arrayOfExistingAccounts.getAccount();
+			accounts.add(account);
+			logger.debug("Account(code=" + String.valueOf(nMax) + ") has been created succesfully.");
+			return account;
+		}catch(Exception e){
+			e.printStackTrace();
+		}
+		
 		return null;
 	}
 	
