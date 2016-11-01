4e0ac5e311557b51588a6d7f9a6bd9eec0922d8b
yorktips <york.tips@gmail.com>
2016-10-31 23:07:47 -0400

    edit

--- src/test/java/com/isis/util/xero/AppTest.java
+++ src/test/java/com/isis/util/xero/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        System.out.println("Updated");
+        System.out.println("edit");
     }
 
     /**
