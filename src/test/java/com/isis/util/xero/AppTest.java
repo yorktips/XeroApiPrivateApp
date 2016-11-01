f754a948fdd5d5b3a338a0f3adbe0c301a45ca37
yorktips <york.tips@gmail.com>
2016-10-31 23:42:49 -0400

    test

--- src/test/java/com/isis/util/xero/AppTest.java
+++ src/test/java/com/isis/util/xero/AppTest.java
@@ -18,7 +18,7 @@ public class AppTest
     public AppTest( String testName )
     {
         super( testName );
-        System.out.println("test fetch");
+        System.out.println("test fetch2");
     }
 
     /**
